e54ca79d4132dc3e2bcc7826037273eefbe0e69d
Syed Asil Ali <asilshah17@gmail.com>
2022-10-31 22:25:35 +0500

    Chapter 5: IF Conditional Statements

--- main.js
+++ main.js
@@ -1,6 +1,41 @@
-let myName = "Fuck";
 
-console.log(myName.charAt(Math.floor(Math.random() * myName.length)));
-console.log(myName.charAt(Math.floor(Math.random() * myName.length)));
-console.log(myName.charAt(Math.floor(Math.random() * myName.length)));
-console.log(myName.charAt(Math.floor(Math.random() * myName.length)));
+// Conditionals : IF Statements
+
+//Syntax
+
+let num = 9;
+
+let reply;
+
+if(num%2==0){
+    reply = "Even Number";
+    
+}
+else{
+    reply="Odd Number";
+}
+console.log(reply);
+
+let score = 59;
+let grade;
+let collegeStudent = false;
+
+if(score >=90){
+    grade = 'A';
+}else if(score >=80){
+    grade = 'B';
+}else if(score >=70){
+    grade = 'C';
+}else if(score >=60){
+    grade = 'D';
+}else{
+    if(collegeStudent){
+        grade = 'U';
+    }
+    else{
+        grade = 'F'
+    }
+}
+console.log(grade);
+
+
