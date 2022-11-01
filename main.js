7adfaf5477687cb725f9396435aa949b992d706d
Syed Asil Ali <asilshah17@gmail.com>
2022-11-01 22:31:19 +0500

    Chapter 9: First Game

--- main.js
+++ main.js
@@ -1,16 +1,52 @@
-// User input
+let playerGame = confirm("Do you Want to play?");
 
-//alert("Hello");
+if(playerGame){
 
-// let mybool = confirm("Are you Sure!");
-// console.log(mybool);
+    let computerinput = "rps";
+    let computerChoice = computerinput.charAt(Math.floor(Math.random() * 3)); 
+    
+    if(computerChoice === 'r'){
+        computerChoice ="rock";
+    }else if(computerChoice == 'p'){
+        computerChoice = "paper";
+    }else{
+        computerChoice = "scissors"
+    }
 
-let myname = prompt("Enter Name?")
+    let playerchoice = prompt("Enter Your Choice:");
 
-if(myname){
-    console.log(myname)
-    console.log(myname.length)
-    console.log(myname.trim().length)
-    console.log(myname.trim())
+    if(playerchoice){
+        playerchoice = playerchoice.toLowerCase();
+        console.log("Player Choice :" , playerchoice);
+        console.log("Computer Choice :" , computerChoice);
+
+        if(playerchoice === computerChoice){
+            console.log("Tie Game");
+        }else if(playerchoice === "rock" && computerChoice === "scissors" ){
+            console.log("Player Wins");
+        }
+        else if(playerchoice === "rock" && computerChoice === "paper" ){
+            console.log("computer Wins");
+        }
+        else if(playerchoice === "paper" && computerChoice === "rock" ){
+            console.log("Player Wins");
+        }
+        else if(playerchoice === "paper" && computerChoice === "scissors" ){
+            console.log("computer Wins");
+        }
+        else if(playerchoice === "scissors" && computerChoice === "paper" ){
+            console.log("Player Wins");
+        }
+        else if(playerchoice === "scissors" && computerChoice === "rock"){
+            console.log("computer Wins");
+        }
+    }
+    else{
+        console.log("You didn't  enter your choice!");
+    }
+
 }
- 
+
+else{
+    console.log("Ok Next Time!");
+}
