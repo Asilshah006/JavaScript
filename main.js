e58f6da94eb798f7702244a6c55498109d564eb2
Syed Asil Ali <asilshah17@gmail.com>
2022-11-06 21:09:19 +0500

    Chapter 12: var let const

--- main.js
+++ main.js
@@ -1,51 +1,69 @@
-// Functions
+// var , let and const
 
-function sum(a,b){
-    console.log(a);
-    console.log(b);
 
-    if(b === undefined){
-        return a+a;
+// var x = 2;
+// var x = 3;
+
+// console.log(x);
+
+
+// let y = 2;
+// let y = 3;
+
+// console.log(y);
+
+
+// const x = 1;
+// x =3;
+// console.log(x);
+
+
+// Global Scope
+
+// var x =1;
+// let y = 2;
+// const z = 3;
+
+
+
+// // Local Scope
+// // {
+//         console.log(x);
+// //     let a =2
+// //     console.log(a);
+// // }
+
+// console.log(a);
+
+
+
+
+
+var x = 1 ;
+let y = 2;
+const z = 3;
+
+console.log("Global : " +x);
+console.log("Global : " +y);
+console.log("Global : " +z);
+
+function myFunc(){
+    var x = 10; // Function Scoped
+    const z = 5; // Block Scoped
+
+    
+    {
+        var x = 3;
+        const z = 6;
+        console.log("Block : " +x);
+        console.log("Block : " +y);
+        console.log("Block : " +z);
+        
     }
-    return a+b;
+    console.log("Function : " +x);
+    console.log("Function : " +y);
+    console.log("Function : " +z);
 }
 
-console.log(sum(4))
 
-function getUserNameFromEmail(email){
-return email.slice(0,email.indexOf("@"));
-}
-console.log(getUserNameFromEmail("asilshah17@gmail.com"))
-
-
-
-
-
-
-const getUserNameFromEmail1 = function (email){
-return email.slice(0,email.indexOf("@"));
-}
-
-console.log(getUserNameFromEmail1("Hunainshah17@gmail.com"))
-
-
-
-
-
-
-
-const getUserNameFromEmail2 = (email) =>{
-return email.slice(0,email.indexOf("@"));
-}
-
-console.log(getUserNameFromEmail2("Laraibshah17@gmail.com"))
-
-
-
-
-
-const toProperCase = (string) =>{
-    return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
-}
-
-console.log(toProperCase("september"))
+myFunc();
