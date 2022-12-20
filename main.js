914f8b9e53f879b56f64eb053408faa1c21b2555
Syed Asil Ali <asilshah17@gmail.com>
2022-12-20 23:13:23 +0500

    Chapter 18 : Classes

--- main.js
+++ main.js
@@ -1,98 +1,119 @@
-// let myobj = {
-//     name : "Dave",
-//     answer : 42,
-//     alive : true,
-//     Hobbies : ["Eat" , "Sleep" , "Code"],
-//     Beverages : {
-//         morning : "Coffee",
-//         afternoon : "Iced Tea"
-//     },
-//         action : function() {
-//         return `Time for ${myobj.Beverages.afternoon}`;
-//     } 
-
-    
-// };
-
-//     console.log(myobj.action());
-
-
-// const vehicle = {
-//     wheels: 4,
-//     engine : function(){
-//         return "Vrrrooom";
+// class Pizza {
+//     constructor(size , type){
+//         this.size  = size;
+//         this.crust = "Original";
+//         this.type = type;
+//         this.toppings = [];
 //     }
 
+//     getCrust(){
+//         return this.crust;
+//     }
+
+//     setCrust(crust){
+//         this.crust = crust;
+//     }
+
+//     setToppings(toppings){
+//         this.toppings.push(toppings);
+//     }
+
+//     getToppings(){
+//         return this.toppings;
+//     }
+
+//     bake(){
+//         return `Baking a ${this.size} size ${this.type} ${this.crust} crust pizza`;
+//     }
 // }
 
-// const truck = Object.create(vehicle);
+// const mypizza = new Pizza("Small" , "Supreme");
+//     mypizza.setCrust("thin");
+//     console.log(mypizza.bake());
+//     mypizza.setToppings("Olives");
+//     mypizza.setToppings("Sausage");
+//     console.log(mypizza.getCrust());
+//     console.log(mypizza.getToppings);
 
-// truck.doors = 2;
-// console.log(truck.wheels);
-// console.log(truck.doors);
-// console.log(truck.engine());
+// class Pizza {
+//     constructor(size){
+//         this.size  = size;
+//         this.crust = "Original";
+       
+//     }
 
+//     bake(){
+//         return `Baking a ${this.size} size ${this.type} ${this.crust} crust pizza`;
+//     }
 
+//     setCrust(crust){
+//         this.crust = crust;
+//     }
 
-
-// const car = Object.create(vehicle);
-
-// car.doors = 4;
-// car.engine = function() { 
-//     return "Whooosh";
-// }
-
-// console.log(car.doors);
-// console.log(car.engine());
-
-
-
-
-
-// const tesla = Object.create(car);
-
-// tesla.engine = function(){
-//     return "Sssshhhhh";
-// }
-
-// console.log(tesla.wheels);
-// console.log(tesla.engine());
-
-
-const band = {
-    vocals : "Robert plant",
-    guitar : "Jimmy Page",
-    bass : "John Paul Jones",
-    drums : "John Boonham" 
-};
-
-// delete band.drums;
-// console.log(band.hasOwnProperty("drums"));
-
-// console.log(Object.keys(band));
-// console.log(Object.values(band));
-
-// for (let job in band){
-//     //console.log(band[job]);
-//     console.log(`On ${job}, its ${band[job]} `);
+//     getCrust(){
+//         return this.crust;
+//     }
 // }
 
 
+// //Inheritence
+// class SpecialityPizza extends Pizza{
+//     constructor(size){
+//         super(size);
+//         this.type  = "The Works";
+//     }
+//     slice(){
+//         console.log(`Our ${this.type} ${this.size} size pizza have 8 slices`);
+//     }
+// }
+
+// const mySpecial = new SpecialityPizza("medium");
+// mySpecial.slice();
 
 
-// destructuring Objects
 
-// const {guitar : myvariable , vocals : vocal} = band;
 
-// console.log(myvariable);
-// console.log(vocal);
+//Factory Funtion for Private fields
 
-const {vocals , guitar , bass , drums} = band;
+// function pizzaFactory(pizzaSize){
+//     const crust = "Original";
+//     const size = pizzaSize;
+    
+//     return{
+//         bake: () => console.log(`Baking a ${size} size ${crust} crust Pizza`)
+//     };
+// }
 
-console.log(vocals);
+// const pizza = pizzaFactory("small");
+// pizza.bake();
 
-function sings({vocals}){
-    return `${vocals} Sings!`;
+
+
+
+
+class Pizza {
+    crust = "original";
+    #sauce = "Traditional";
+    #size;
+    constructor(size){
+        this.#size  = size;
+    }
+
+    bake(){
+        return `Baking a ${this.size} size ${this.type} ${this.#sauce} sauce ${this.crust} crust pizza`;
+    }
+
+    setCrust(crust){
+        this.crust = crust;
+    }
+
+    getCrust(){
+        return this.crust;
+    }
 }
 
-console.log(sings(band));
+const pi = new Pizza("small");
+
+ console.log(pi.bake());
+    console.log(pi.size);
+    console.log(pi.sauce);
