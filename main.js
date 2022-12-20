b51899cfd1f99f26cede4074c70eaa4e4f82710a
Syed Asil Ali <asilshah17@gmail.com>
2022-12-20 23:26:29 +0500

    Chapter 19 : JSON

--- main.js
+++ main.js
@@ -1,119 +1,22 @@
-// class Pizza {
-//     constructor(size , type){
-//         this.size  = size;
-//         this.crust = "Original";
-//         this.type = type;
-//         this.toppings = [];
-//     }
-
-//     getCrust(){
-//         return this.crust;
-//     }
-
-//     setCrust(crust){
-//         this.crust = crust;
-//     }
-
-//     setToppings(toppings){
-//         this.toppings.push(toppings);
-//     }
-
-//     getToppings(){
-//         return this.toppings;
-//     }
-
-//     bake(){
-//         return `Baking a ${this.size} size ${this.type} ${this.crust} crust pizza`;
-//     }
-// }
-
-// const mypizza = new Pizza("Small" , "Supreme");
-//     mypizza.setCrust("thin");
-//     console.log(mypizza.bake());
-//     mypizza.setToppings("Olives");
-//     mypizza.setToppings("Sausage");
-//     console.log(mypizza.getCrust());
-//     console.log(mypizza.getToppings);
-
-// class Pizza {
-//     constructor(size){
-//         this.size  = size;
-//         this.crust = "Original";
-       
-//     }
-
-//     bake(){
-//         return `Baking a ${this.size} size ${this.type} ${this.crust} crust pizza`;
-//     }
-
-//     setCrust(crust){
-//         this.crust = crust;
-//     }
-
-//     getCrust(){
-//         return this.crust;
-//     }
-// }
-
-
-// //Inheritence
-// class SpecialityPizza extends Pizza{
-//     constructor(size){
-//         super(size);
-//         this.type  = "The Works";
-//     }
-//     slice(){
-//         console.log(`Our ${this.type} ${this.size} size pizza have 8 slices`);
-//     }
-// }
-
-// const mySpecial = new SpecialityPizza("medium");
-// mySpecial.slice();
-
-
-
-
-//Factory Funtion for Private fields
-
-// function pizzaFactory(pizzaSize){
-//     const crust = "Original";
-//     const size = pizzaSize;
-    
-//     return{
-//         bake: () => console.log(`Baking a ${size} size ${crust} crust Pizza`)
-//     };
-// }
-
-// const pizza = pizzaFactory("small");
-// pizza.bake();
-
-
-
-
-
-class Pizza {
-    crust = "original";
-    #sauce = "Traditional";
-    #size;
-    constructor(size){
-        this.#size  = size;
-    }
-
-    bake(){
-        return `Baking a ${this.size} size ${this.type} ${this.#sauce} sauce ${this.crust} crust pizza`;
-    }
-
-    setCrust(crust){
-        this.crust = crust;
-    }
-
-    getCrust(){
-        return this.crust;
+const myobj ={
+    name : "Asil",
+    answer : 42,
+    Hobbies : ["Eat" , "Sleep" , "Code"],
+    action : function(){
+        return "Hello";
     }
 }
 
-const pi = new Pizza("small");
+console.log(myobj.name);
+console.log(myobj.answer);
+console.log(myobj.Hobbies);
+console.log(myobj.action());
 
- console.log(pi.bake());
-    console.log(pi.size);
-    console.log(pi.sauce);
+const sendJSON = JSON.stringify(myobj);
+
+console.log(sendJSON);
+console.log(typeof sendJSON);
+
+const recieveJSON = JSON.parse(sendJSON);
+console.log(recieveJSON);
+console.log(typeof recieveJSON);
