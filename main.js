b47fb93edb07ec76b75f7ab300427f678ba4d2be
Syed Asil Ali <asilshah17@gmail.com>
2022-12-21 22:51:47 +0500

    Chapter 20 : Error Handling

--- main.js
+++ main.js
@@ -1,22 +1,61 @@
-const myobj ={
-    name : "Asil",
-    answer : 42,
-    Hobbies : ["Eat" , "Sleep" , "Code"],
-    action : function(){
-        return "Hello";
+
+"use strict";
+
+// variable = "asil";
+
+
+// const abc = 584;
+
+// abc = 58;
+
+
+//Object..create();
+
+// const makeError = ()=> {
+//     try{
+//         const name = "Dave";
+//         name = "Joe";
+//     }catch(err){
+//         console.error(err.message);
+//     }
+// }
+
+
+
+function customError(message){
+    this.message = message;
+    this.name = "CustomError";
+    this.stack = `${this.name} ${this.message} `
+}   
+
+// const makeError = ()=> {
+//     try{
+//        throw new customError("This is Error");
+//     }catch(err){
+//         console.error(err.message);
+//         console.log(err.name);
+//         console.log(err.stack);
+//     }
+// }
+
+
+const makeError = ()=> {
+    let i = 1;
+    while(i <= 5){
+
+    try{
+        if(i % 2 !== 0){
+       throw new customError("This is Odd Number");
+        }
+        console.log("Even Number");
+    }catch(err){
+        console.error(err.stack);
+       
+    }finally{
+        console.log("...Finally");
+        i++;
     }
-}
+    } 
+};
 
-console.log(myobj.name);
-console.log(myobj.answer);
-console.log(myobj.Hobbies);
-console.log(myobj.action());
-
-const sendJSON = JSON.stringify(myobj);
-
-console.log(sendJSON);
-console.log(typeof sendJSON);
-
-const recieveJSON = JSON.parse(sendJSON);
-console.log(recieveJSON);
-console.log(typeof recieveJSON);
+makeError();
