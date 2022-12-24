12da5a48de7c7697e89d39225e60780ae1029134
Syed Asil Ali <asilshah17@gmail.com>
2022-12-24 10:13:26 +0500

    Chapter 21 : Document Object Model (DOM)

--- main.js
+++ main.js
@@ -1,61 +1,89 @@
+// Document Object Modelling
 
-"use strict";
+const view1 = document.getElementById("view1");
 
-// variable = "asil";
+console.log(view1);
+
+const view2 = document.querySelector("#view2");
+
+console.log(view2);
+
+view1.style.display = "flex";
+view2.style.display = "none";
+
+const views = document.getElementsByClassName("view");
+console.log(views);
+
+const sameViews = document.querySelectorAll(".view");
+console.log(sameViews);
+
+const divs = view1.querySelectorAll("div");
+console.log(divs);
+
+const sameDivs = view1.getElementsByTagName("div");
+console.log(sameDivs);
+
+const evenDivs = view1.querySelectorAll("div:nth-of-type(2n)");
+console.log(evenDivs);
+
+for (let i = 0 ; i<evenDivs.length ; i++){
+    evenDivs[i].style.backgroundColor = "darkblue";
+    evenDivs[i].style.width = "100px";
+    evenDivs[i].style.height = "100px";
+}
+
+const navText = document.querySelector("nav h1");
+console.log(navText);
+
+navText.textContent = "Hello World";
+
+const navBar = document.querySelector("nav");
+navBar.innerHTML =  `<h1>Hello</h1> <p>This should be align right.</p>`
+console.log(navBar);
 
 
-// const abc = 584;
+//Navigating DOM tree
 
-// abc = 58;
-
-
-//Object..create();
-
-// const makeError = ()=> {
-//     try{
-//         const name = "Dave";
-//         name = "Joe";
-//     }catch(err){
-//         console.error(err.message);
-//     }
-// }
-
-
-
-function customError(message){
-    this.message = message;
-    this.name = "CustomError";
-    this.stack = `${this.name} ${this.message} `
-}   
-
-// const makeError = ()=> {
-//     try{
-//        throw new customError("This is Error");
-//     }catch(err){
-//         console.error(err.message);
-//         console.log(err.name);
-//         console.log(err.stack);
-//     }
-// }
+console.log(evenDivs[0]);
+console.log(evenDivs[0].parentElement);
+console.log(evenDivs[0].parentElement.children);
+console.log(evenDivs[0].parentElement.childNodes);
+console.log(evenDivs[0].parentElement.hasChildNodes());
+console.log(evenDivs[0].parentElement.lastChild);
+console.log(evenDivs[0].parentElement.lastElementChild);
+console.log(evenDivs[0].parentElement.firstChild);
+console.log(evenDivs[0].parentElement.firstElementChild);
+console.log(evenDivs[0].nextSibling);
+console.log(evenDivs[0].nextElementSibling.nextElementSibling);
+console.log(evenDivs[0].previousSibling);
+console.log(evenDivs[0].previousElementSibling);
 
 
-const makeError = ()=> {
-    let i = 1;
-    while(i <= 5){
+view1.style.display = "none";
+view2.style.display = "flex";
+view2.style.flexDirection = "row";
+view2.style.flexWrap = "wrap";
+view2.style.margin = "10px";
 
-    try{
-        if(i % 2 !== 0){
-       throw new customError("This is Odd Number");
-        }
-        console.log("Even Number");
-    }catch(err){
-        console.error(err.stack);
-       
-    }finally{
-        console.log("...Finally");
-        i++;
-    }
-    } 
-};
+while(view2.lastChild){
+    view2.lastChild.remove();
+}
 
-makeError();
+const createDivs = (parent , iter) =>{
+    const newDiv = document.createElement("div");
+    newDiv.textContent = iter;
+    newDiv.style.backgroundColor = "#000";
+    newDiv.style.width = "100px";
+    newDiv.style.height = "100px";
+    newDiv.style.margin = "10px"
+    newDiv.style.display = "flex";
+    newDiv.style.justifyContent = "center"; 
+    newDiv.style.alignItems = "center"; 
+    newDiv.style.color = "#fff"
+    parent.append(newDiv);
+}
+
+for (let i = 1 ; i<=12 ; i++){
+    createDivs(view2 , i);
+}
+
