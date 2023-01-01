a83b01da92ec8617359d6e715d63b1418e111745
Syed Asil Ali <asilshah17@gmail.com>
2023-01-01 10:15:41 +0500

    Chapter 24 : Modules

---
+++ guitars.js
@@ -0,0 +1,13 @@
+export function playGuitar(){
+    console.log("Playing Guitar");
+}
+export const shredding = () =>{
+    console.log("Shredding some licks");
+}
+
+export const plucking = () =>{
+    console.log("Plucking the Strings...");
+}
+
+// export default playGuitar;
+// export {shredding , plucking};
