29a28ef4b702bde90bd604c888d68027268187f8
Syed Asil Ali <asilshah17@gmail.com>
2023-01-01 09:37:37 +0500

    Chapter 23 : Web Storage API

--- main.js
+++ main.js
@@ -1,107 +1,26 @@
-const view1 = document.querySelector("#view1");
-// console.log(view1);
+const view = document.querySelector("#view1");
 
-view1.style.display = "none";
+view.style.display = "flex";
 
-const view2 = document.getElementById("view2");
-// console.log(view2);
+const myArray = ["eat" , "sleep" , "code" ];
 
-//view2.style.display = "flex";
-view2.style.display = "none";
-
-const vie3 = document.querySelector("#view3");
-vie3.style.display ="flex";
-
-
-
-
-
-
-
-
-// Syntax : addEventListener("click , function , useCapture");
-
-// const doSomething = () =>{
-//     alert("doing Something");
-// }
-
-// //h2.addEventListener("click" , doSomething , false);
-// // h2.removeEventListener("click" , doSomething , false);
-
-// h2.addEventListener("click", (event) => {
-//     console.log(event.target);
-//     event.target.textContent = "Clicked";
-// } );
-
-
-document.addEventListener("readystatechange" , (event) =>{
-    if(event.target.readyState === "complete"){
-        console.log("readystate : complete");
-        initApp();
+const myObj = {
+    name : "ASil",
+    hobbies: ["eat" , "sleep" , "code" ],
+    logname : function(){
+        console.log(
+        this.name
+        );
     }
-} );
-
-// const initApp = () => {
-//     const view = document.querySelector("#view2");
-//     const div = view.querySelector("div");
-//     const h2 = div.querySelector("h2");
-
-//     view.addEventListener("click" , (event) =>{
-    
-//         //view.style.backgroundColor = "purple";
-
-//         view.classList.toggle("purple");
-//         view.classList.toggle("darkblue");
-
-//     }, 
-//     false );
-
-//     div.addEventListener("click" , (event) =>{
-        
-//         //div.style.backgroundColor = "blue";
-
-//         div.classList.toggle("blue");
-//         div.classList.toggle("black");
-
-//     },
-//     false );
-
-//     h2.addEventListener("click" , (event) =>{
-//         //event.stopPropagation();
-//        //event.target.textContent = "Clicked";
- 
-//         let mytext = event.target.textContent;
-
-//         mytext === "My 2nd View"
-//         ? (event.target.textContent = "Clicked")
-//         : (event.target.textContent = "My 2nd View"); 
- 
-//     },
-//     false );
-
-// }
-
-//     const nav = document.querySelector("nav");
-
-//     nav.addEventListener("mouseover", (event) => {
-//         event.target.classList.add("height100");
-//     }
-//     ,false);
-
-//     nav.addEventListener("mouseout" , (event) =>{
-//     event.target.classList.remove("height100");        
-//     }
-//     ,false);
-
-const initApp = () =>{
-    const view3 = document.querySelector("#view3");
-
-    const myform = view3.querySelector("#myForm");
-
-    myform.addEventListener("submit" , (event) =>{
-        event.preventDefault();
-        console.log("Submit Event");
-    },
-    false)
-
 }
+
+
+localStorage.setItem("mySessionStore" ,JSON.stringify(myObj));
+//localStorage.removeItem("mySessionStore");
+//localStorage.clear();
+const key = localStorage.key(0);
+const length = localStorage.length;
+console.log(key);
+console.log(length);
+const mydata = JSON.parse(localStorage.getItem("mySessionStore"));
+console.log(mydata);
